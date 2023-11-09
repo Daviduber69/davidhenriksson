e703b21a52887b9bbeed1dbbc51aec0e4bf712f4
David Henriksson <davhen0108@skola.goteborg.se>
2023-11-09 20:19:02 +0100

    added method calls to the start() method

--- Guesser.java
+++ Guesser.java
@@ -35,7 +35,9 @@ public class Guesser{
    */
   public void start(){
     // call the rules method here
-    // call the doGuesses() method here
+    rules();
+// call the doGuesses() method here
+    doGuesses();
   }
 
   private void rules(){
