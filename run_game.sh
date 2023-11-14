3b03f43b7a84122809830ce6beca7c73b46cdd13
David Henriksson <davhen0108@skola.goteborg.se>
2023-11-14 13:40:33 +0100

    created script to do lots of cool things

---
+++ run_game.sh
@@ -0,0 +1,14 @@
+echo David Henriksson
+mkdir David_Henriksson_labb
+mv Guesser.java David_Henriksson_labb
+cd David_Henriksson_labb
+pwd
+echo Compiling...
+javac Guesser.java
+echo running game...
+java Guesser
+echo Done!
+echo Removing class files...
+rm *.class
+ls
+
