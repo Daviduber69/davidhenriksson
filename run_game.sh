2c9f893ddd6b5a95514db8c109569a597ee686af
David Henriksson <davhen0108@skola.goteborg.se>
2023-11-15 11:03:33 +0100

    altered the script file to work better

--- run_game.sh
+++ run_game.sh
@@ -1,12 +1,14 @@
 echo David Henriksson
 mkdir David_Henriksson_labb
-mv Guesser.java David_Henriksson_labb
+cp Guesser.java David_Henriksson_labb
+cp GuessingGame.java David_Henriksson_labb
 cd David_Henriksson_labb
 pwd
 echo Compiling...
 javac Guesser.java
-echo running game...
-java Guesser
+javac GuessingGame.java
+echo Running game...
+java GuessingGame
 echo Done!
 echo Removing class files...
 rm *.class
